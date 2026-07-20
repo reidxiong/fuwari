f68b94cf2080d141a8cd6ac86126ae584753a76c
reid xiong <cuicuixiong97@gmail.com>
2026-07-20 17:19:11 +0800

    Update profileConfig with new name and bio

--- src/config.ts
+++ src/config.ts
@@ -54,8 +54,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "踏川",
+	bio: "踏川中文.",
 	links: [
 		{
 			name: "Twitter",
