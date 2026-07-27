906d879e70575b8ef2efb753f841c2a2f5e4d790
reid xiong <cuicuixiong97@gmail.com>
2026-07-27 13:13:21 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -63,13 +63,6 @@ export const profileConfig: ProfileConfig = {
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
 			url: "https://twitter.com",
-		},
-		{
-			
-		},
-		{
-		
-		},
 	],
 };
 
