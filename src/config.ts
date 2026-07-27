1a52c6b74c578c7686a9f41fc06cbb5931ff8c93
reid xiong <cuicuixiong97@gmail.com>
2026-07-27 12:18:54 +0800

    Remove Steam and GitHub entries from config
    Removed Steam and GitHub entries from the config.

--- src/config.ts
+++ src/config.ts
@@ -65,14 +65,10 @@ export const profileConfig: ProfileConfig = {
 			url: "https://twitter.com",
 		},
 		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			
 		},
 		{
-			name: "GitHub",
-			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+		
 		},
 	],
 };
